1c1587776a463732a982b0cac1974f196833b40b
colmogara <36032384+colmogara@users.noreply.github.com>
2018-03-21 20:25:43 +0000

    Add files via upload

---
+++ lcdForPillbox.ino
@@ -0,0 +1,30 @@
+#include <Wire.h>
+#include "rgb_lcd.h"
+
+rgb_lcd lcd;
+
+const int colorR = 0;
+const int colorG = 0;
+const int colorB = 50;
+
+void setup() 
+{
+    // set up the LCD's number of columns and rows:
+    lcd.begin(16, 2);
+    
+    lcd.setRGB(colorR, colorG, colorB);
+    // Print a message to the LCD.
+    lcd.print("Please Take Your Pills!");
+    
+    delay(1000);
+}
+
+void loop() 
+{
+    // set the cursor to column 0, line 1
+    // (note: line 1 is the second row, since counting begins with 0):
+    lcd.setCursor(0, 1);
+    int time = millis()/1000;
+    
+
+}
